9249fde1ad636ce6c460fe9585d3d9d0f506a7bd
GeriDimitrova24 <geridimirova24@gmail.com>
2024-04-09 15:58:22 +0300

    ex1

---
+++ Ex.js
@@ -0,0 +1,15 @@
+let number;
+if (number) {
+console.log(number)
+} else {
+console.log('false') 
+}
+
+let person = {
+    firstName:"John",
+    lastName:"Doe",
+    age:50
+    };
+    person = null;
+    console.log(person); 
+    console.log(typeof(person));
