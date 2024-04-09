c82efb3ab4203f42da9755dc240a0b8c724e02e5
GeriDimitrova24 <geridimirova24@gmail.com>
2024-04-09 16:31:18 +0300

    ex.js

--- Ex.js
+++ Ex.js
@@ -13,3 +13,31 @@ let person = {
     person = null;
     console.log(person); 
     console.log(typeof(person));
+
+function SumFirstandLastEl(el) {
+    let firstEl=+(el[0]);
+    let lastEl=+(el[el.lenght - 1]);
+    let sum=firstEl + lastEl;
+    console.log(sum);
+    console.log(typeof sum);
+    }
+SumFirstandLastEl(['20','30','50']);
+    
+function DaysofWeek(n) {
+    const days = [
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday", 
+        "Sunday" 
+    ]
+    if (n >=1 && n<=7) {
+         console.log(days[n - 1]);
+    }
+    else {
+        console.log('Invalid Day!');
+    }
+}
+DaysofWeek(5);
